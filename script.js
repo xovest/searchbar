152e82f32f1d8410fbf86d4a0f498c9c643265b8
xovest <79008917+xovest@users.noreply.github.com>
2022-02-04 12:03:19 +0200

    srcbr

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const userCardTempalte = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", (e) => {
+  const val = e.target.value.toLowerCase()
+  users.forEach(user => {
+    const isVisible = user.name.toLowerCase().includes(val) || user.email.toLowerCase().includes(val)
+    user.element.classList.toggle("hide", !isVisible)
+  })
+})
 
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
   .then(data => {
-    data.forEach(user => {
+    users = data.map(user => {
       const card = userCardTempalte.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
       header.textContent = user.name
       body.textContent = user.email
       userCardContainer.append(card)
+      return {
+        name: user.name,
+        email: user.email,
+        element: card
+      }
     })
   })
